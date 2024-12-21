d079a652773f6bd13277dcf5d616de38f128b134
arvie-reyhan <reyhan.arvie@gmail.com>
2024-12-22 06:48:08 +0700

    feat: login page and splash screen

---
+++ components/ButtonAuth.jsx
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+function Button({bgColor = "#19918F", text, navigation, onPress}) {
+    return(
+        <TouchableOpacity style={{ ...styles.button, backgroundColor: bgColor }} onPress={onPress} >
+        <Text style={styles.buttonText}>{text}</Text>
+      </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    button: {
+      paddingVertical: 15,
+      paddingHorizontal: 30,
+      borderRadius: 15,
+      width: "100%",
+      alignItems: "center",
+    },
+    buttonText: {
+      color: "#fff",
+      fontSize: 16,
+      fontWeight: "bold",
+    },
+  });
+
+export default Button
